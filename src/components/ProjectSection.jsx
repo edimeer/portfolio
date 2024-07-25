af5088eafe10c210ad56dde1112e85689c9359f5
Edi Edwardson C. Meer <edison.cmeer@gmail.com>
2024-07-25 15:04:40 +0800

    Updated Project description

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -8,11 +8,14 @@ const ProjectSection = () => {
       </div>
       <div className="block max-w border border-cMustard rounded-lg shadow">
         <img className="w-max rounded-lg" src={carelever} alt="profile" />
-        <div className="p-6 space-y-2">
+        <div className="p-6 space-y-3">
           <h5 className="text-lg font-bold text-gray-700">
             Carelever
           </h5>
-          <p className="mb-3 font-normal text-gray-700">A cloud-based SaaS platform that makes it easier for you to manage your occupational health processes and health data.</p>
+          <p className="mb-3 font-normal text-gray-700 italic">A cloud-based SaaS platform that makes it easier for you to manage your occupational health processes and health data.</p>
+          <p className="mb-3 font-normal text-gray-700">
+            As part of a dedicated team of full stack developers, I contributed to the development of Carelever. This platform helps over 500 companies manage their occupational health programs more effectively. My role involved working with Ruby on Rails for the backend and Angular for the frontend, focusing on backend development, API integration, and enhancing user interface features.
+          </p>
         </div>
       </div>
     </section>
